dab54e511b2da25ee3f8ceb92147d2e514d35cd1
dadal00 <dylanadal.00@gmail.com>
2025-11-10 16:12:06 -0500

    stripped down compute

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use pollster::FutureExt;
 
 fn main() {
     env_logger::init();
-    #[cfg(feature = "introduction")]
-    camera::introduction::run().block_on().unwrap();
     #[cfg(feature = "sort")]
     camera::sort::run().block_on().unwrap();
 }
